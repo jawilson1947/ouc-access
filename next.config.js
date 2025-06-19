7223209d27c9e247e1e16aad06bd2d27c20b2ef7
Jim Wilson <jawilson1947@gmail.com>
2025-06-19 01:43:45 -0500

    picture handling fixes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/images/**',
+      },
+      {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
         port: '',
